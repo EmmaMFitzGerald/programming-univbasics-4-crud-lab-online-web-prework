4bcbbc303b0023057e9e12c4f1f1dc5c7a01747e
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2019-06-18 18:43:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["Emma", "Holly", "Sophie", "Nolan"]
-  element = "Norman"
-  array << element
+  
 end
 
 def add_element_to_start_of_array(array, element)
